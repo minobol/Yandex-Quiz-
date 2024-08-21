24beceafcf79fa7a7757c5894c3a02cc8408b162
minobol <120034142+minobol@users.noreply.github.com>
2024-08-21 20:38:23 +0300

    Delete YandexQuiz directory

--- YandexQuiz/Services/QuestionFactoryDelegate.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-
-protocol QuestionFactoryDelegate: AnyObject {
-    func didReceiveNextQuestion(question: YandexQuestion?)
-    func didLoadDataFromServer()
-    func didFailToLoadData(with error: Error)
-}
